740b1200e39307bbaf5e75b691d22a25d11906d4
Andrey Tsykunov <andrey.tsykunov@gmail.com>
2018-11-04 16:46:49 -0500

    added test to listen for value updates

--- kvstore-impl/src/test/scala/com/andrey/playground/kvstore/impl/KVStoreServiceAcceptanceTest.scala
+++ kvstore-impl/src/test/scala/com/andrey/playground/kvstore/impl/KVStoreServiceAcceptanceTest.scala
@@ -1,19 +1,25 @@
 package com.andrey.playground.kvstore.impl
 
+import akka.Done
 import akka.actor.ActorSystem
 import akka.stream.Materializer
-import com.andrey.playground.kvstore.api.{AkkaComponents, KVStoreService}
+import akka.stream.scaladsl.Sink
+import com.andrey.playground.kvstore.api.{AkkaComponents, KVStoreService, ValueUpdatedMessage}
 import com.lightbend.lagom.scaladsl.api.ServiceLocator
 import com.lightbend.lagom.scaladsl.client.{ConfigurationServiceLocatorComponents, LagomClientApplication, ServiceClient}
 import com.typesafe.config.Config
 import com.typesafe.scalalogging.LazyLogging
-import org.scalatest.{AsyncFunSuite, Matchers}
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.{Seconds, Span}
+import org.scalatest.{AsyncFunSuite, FunSuite, Matchers}
 import play.api.Configuration
 import play.api.libs.ws.ahc.AhcWSComponents
 
 import scala.concurrent.ExecutionContext
 
-class KVStoreServiceAcceptanceTest extends AsyncFunSuite with Matchers with LazyLogging {
+class KVStoreServiceAcceptanceTest extends AsyncFunSuite with Matchers with LazyLogging with ScalaFutures {
+
+  //implicit val pc = PatienceConfig(Span(2, Seconds))
 
   logger.info("Starting")
 
@@ -21,8 +27,29 @@ class KVStoreServiceAcceptanceTest extends AsyncFunSuite with Matchers with Lazy
 
   test("can get key value") {
 
-    app.kvStoreService.get("alice").invoke().map { it =>
+    val f = app.kvStoreService.get("alice").invoke()
+
+    //whenReady(f) { it =>
+    f.map { it =>
       it should not be empty
+    }
+  }
+
+  ignore("subscribe to key value updates") {
+    val source = app.kvStoreService.updatesTopic().subscribe
+      .withGroupId("KVStoreServiceAcceptanceTest")
+      .atMostOnceSource
+
+    implicit val m = app.materializer
+
+    val log = Sink.foreach[ValueUpdatedMessage] { v =>
+      logger.debug(s"Received $v")
+    }
+
+    source.runWith(log).map { it =>
+
+      // never completes
+      it shouldBe Done
     }
   }
 
--- kvstore-impl/src/test/scala/com/andrey/playground/kvstore/impl/LagomClientApp.scala
+++ kvstore-impl/src/test/scala/com/andrey/playground/kvstore/impl/LagomClientApp.scala
@@ -1,12 +1,14 @@
 package com.andrey.playground.kvstore.impl
 
 import com.andrey.playground.kvstore.api.AkkaComponents
+import com.lightbend.lagom.scaladsl.broker.kafka.LagomKafkaClientComponents
 import com.lightbend.lagom.scaladsl.client.{ConfigurationServiceLocatorComponents, LagomClientApplication}
 import play.api.Configuration
 import play.api.libs.ws.ahc.AhcWSComponents
 
 class LagomClientApp(name: String) extends LagomClientApplication(name)
   with ConfigurationServiceLocatorComponents
+  with LagomKafkaClientComponents
   with AhcWSComponents
   with AkkaComponents {
 
